c745cf98ee439e2fd9e9de7cfd8683c05c751c5d
CesarRomeroDev <cesar_romero.dev@outlook.com>
2021-07-05 23:42:00 -0500

    comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -9,11 +9,20 @@
 
 
 # if __name__ == '__main__':
-#     run()
+    #run()
 
 def run():
     squares = [i**2 for i in range(1, 101) if i % 3 != 0] # Para cada i en el rango que va de 1 a 101 , voy a guardar esa i elevada al cuadrado, solamente si la i modulo 3 es distinto de 0 
     print(squares)
+
+
+
+if __name__ == '__main__':
+    run()
+
+# def run():
+#     squares = [i for i in range(1, 101) if i % 3 == 0 and  i % 6 == 0 and i % 9 == 0]
+#     print(squares)
 
 
 
